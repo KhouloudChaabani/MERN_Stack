f9e51bf71302f7958b3b2fbe191fd1efec3473d3
KhouloudChaabani <khouloudchaabani33@gmail.com>
2023-07-03 16:15:29 +0100

    first_react

---
+++ ReactV18/practice/first-project/src/Components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <h1>Hello Dojo</h1>
+        <p>Things I need to do:</p>
+        <ul>
+            <li>Learn React</li>
+            <li>Climb Mt.Everest</li>
+            <li>Run a marathon</li>
+            <li>Feed the dogs</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Home
